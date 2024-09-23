f147e26d2ced2d5f1680dd2476d2f25ffbf5ef74
aahan <aahannutella@gmail.com>
2024-09-22 20:55:55 -0700

    auto setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/autoPrototype.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/autoPrototype.java
@@ -2,12 +2,32 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
-public class autoPrototype {
-    public void init(){
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+public class autoPrototype extends LinearOpMode {
+
+    private DcMotor frontLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor backRightDrive = null;
+
+    public void initialize(){
         //telemetry init
         telemetry.addData("Status", "Initializing");
         telemetry.update();
         //
 
     }
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+
 }
