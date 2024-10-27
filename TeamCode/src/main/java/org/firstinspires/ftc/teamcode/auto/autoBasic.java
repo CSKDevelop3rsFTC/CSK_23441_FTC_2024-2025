28acdd355102452f50d0d201b63135025a476df4
aahan <aahannutella@gmail.com>
2024-10-27 09:50:22 -0700

    auto basic ready

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/autoBasic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/autoBasic.java
@@ -19,27 +19,46 @@ public class autoBasic extends LinearOpMode {
     private DcMotor backRightDrive = null;
 
     //4 Stage Slides Motors (Outtake)
-    public DcMotor outtakeMotor1 = null;
-    public DcMotor outtakeMotor2 = null;
+    public DcMotor  outtakeMotor1    = null;
+    public DcMotor  outtakeMotor2    = null;
 
     //Intake Servos
-    public Servo hzfourbarServo1 = null;
-    public Servo hzfourbarServo2 = null;
-    public Servo hzSlidesServo1 = null;
-    public Servo hzSlidesServo2 = null;
-    public CRServo intakeServo = null;
+    public Servo    hzfourbarServo1  = null;
+    public Servo    hzfourbarServo2  = null;
+    public Servo    hzSlidesServo1   = null;
+    public Servo    hzSlidesServo2   = null;
+    public CRServo  intakeServo      = null;
 
     //Outtake Servos
-    public Servo vFourbarServo1 = null;
-    public Servo vFourbarServo2 = null;
-    public Servo clawRotateServo = null;
-    public Servo clawServo = null;
+    public Servo    vFourbarServo1   = null;
+    public Servo    vFourbarServo2   = null;
+    public Servo    clawRotateServo  = null;
+    public Servo    clawServo        = null;
 
-    public double driveSpeed = 0.65;
-    public double intakeSpeed = 0.3;
+    public double driveSpeed = 0.5;
+    public void initialize(){
+        frontLeftDrive    =    hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        frontRightDrive   =   hardwareMap.get(DcMotor.class, "frontRightDrive");
+        backLeftDrive     =     hardwareMap.get(DcMotor.class, "backLeftDrive");
+        backRightDrive    =    hardwareMap.get(DcMotor.class, "backRightDrive");
 
+        outtakeMotor1     =     hardwareMap.get(DcMotor.class, "outtakeMotor1");
+        outtakeMotor2     =     hardwareMap.get(DcMotor.class, "outtakeMotor2");
 
-    public void stopRobot () {
+        hzfourbarServo1   =     hardwareMap.get(Servo.class, "hzfourbarServo1");
+        hzfourbarServo2   =     hardwareMap.get(Servo.class, "hzfourbarServo2");
+        hzSlidesServo1    =      hardwareMap.get(Servo.class, "hzSlidesServo1");
+        hzSlidesServo2    =      hardwareMap.get(Servo.class, "hzSlidesServo2");
+        intakeServo       =       hardwareMap.get(CRServo.class, "intakeServo");
+
+        vFourbarServo1    =      hardwareMap.get(Servo.class, "vFourbarServo1");
+        vFourbarServo2    =      hardwareMap.get(Servo.class, "vFourbarServo2");
+        clawRotateServo   =     hardwareMap.get(Servo.class, "clawRotateServo");
+        clawServo         =           hardwareMap.get(Servo.class, "clawServo");
+
+    }
+
+    public void stopRobot() {
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
         frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -51,20 +70,19 @@ public class autoBasic extends LinearOpMode {
         backLeftDrive.setPower(0);
     }
 
-    public void vertMove ( double dir, int time) { //inputs for dir is -1 or 1 (forward or backward)
+    public void vertMove(double dir, int time){ //inputs for dir is -1 or 1 (forward or backward)
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
-        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
 
         frontRightDrive.setPower(driveSpeed * dir);
         frontLeftDrive.setPower(driveSpeed * dir);
         backRightDrive.setPower(driveSpeed * dir);
         backLeftDrive.setPower(driveSpeed * dir);
         sleep(time);
-        }
-
-    public void horizMove ( double dir, int time){ // -1 for left 1 for positive  for
+    }
+    public void horizMove(double dir, int time){ // -1 for left 1 for positive  for
         frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
         frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -76,8 +94,7 @@ public class autoBasic extends LinearOpMode {
         backLeftDrive.setPower(driveSpeed * dir);
         sleep(time);
     }
-
-    public void pivot ( double dir, int time){ // -1 for turnLeft 1 for turnRight
+    public void pivot(double dir, int time){ // -1 for turnLeft 1 for turnRight
         frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
         backRightDrive.setDirection(DcMotor.Direction.REVERSE);
         frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -89,45 +106,16 @@ public class autoBasic extends LinearOpMode {
         backLeftDrive.setPower(driveSpeed * dir);
         sleep(time);
     }
-    public void spinTake(double dir, int time){
-        intakeServo.setPower(intakeSpeed * dir);
-        sleep(time);
-    }
 
-
-
+    @Override
     public void runOpMode() {
-
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
-        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
-
-        outtakeMotor1 = hardwareMap.get(DcMotor.class, "outtakeMotor1");
-        outtakeMotor2 = hardwareMap.get(DcMotor.class, "outtakeMotor2");
-
-        hzfourbarServo1 = hardwareMap.get(Servo.class, "hzfourbarServo1");
-        hzfourbarServo2 = hardwareMap.get(Servo.class, "hzfourbarServo2");
-        hzSlidesServo1 = hardwareMap.get(Servo.class, "hzSlidesServo1");
-        hzSlidesServo2 = hardwareMap.get(Servo.class, "hzSlidesServo2");
-        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
-
-        vFourbarServo1 = hardwareMap.get(Servo.class, "vFourbarServo1");
-        vFourbarServo2 = hardwareMap.get(Servo.class, "vFourbarServo2");
-        clawRotateServo = hardwareMap.get(Servo.class, "clawRotateServo");
-        clawServo = hardwareMap.get(Servo.class, "clawServo");
-
-        telemetry.addData("Status", "Ready to Start");
+        initialize();
+        telemetry.addData("Status", "Initializing");
         telemetry.update();
-        waitForStart();
 
-        vertMove(1, 700);
-        spinTake(-1,300);
+        vertMove(1,800);
 
         stopRobot();
 
     }
-
-
-
 }
