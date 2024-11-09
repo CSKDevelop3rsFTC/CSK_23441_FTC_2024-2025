5dcbbc1b7493c77ee580e021554d57655860dcb9
p-anumarlu <68264610+p-anumarlu@users.noreply.github.com>
2024-11-08 23:49:35 -0800

    meet0 comm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/colorLocator.java
@@ -0,0 +1,115 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.SortOrder;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+import org.opencv.core.RotatedRect;
+
+import java.util.List;
+
+/*
+ * This OpMode illustrates how to use a video source (camera) to locate specifically colored regions
+ *
+ * Unlike a "color sensor" which determines the color of an object in the field of view, this "color locator"
+ * will search the Region Of Interest (ROI) in a camera image, and find any "blobs" of color that match the requested color range.
+ * These blobs can be further filtered and sorted to find the one most likely to be the item the user is looking for.
+ *
+ * To perform this function, a VisionPortal runs a ColorBlobLocatorProcessor process.
+ *   The ColorBlobLocatorProcessor process is created first, and then the VisionPortal is built to use this process.
+ *   The ColorBlobLocatorProcessor analyses the ROI and locates pixels that match the ColorRange to form a "mask".
+ *   The matching pixels are then collected into contiguous "blobs" of pixels.  The outer boundaries of these blobs are called its "contour".
+ *   For each blob, the process then creates the smallest possible rectangle "boxFit" that will fully encase the contour.
+ *   The user can then call getBlobs() to retrieve the list of Blobs, where each Blob contains the contour and the boxFit data.
+ *   Note: The default sort order for Blobs is ContourArea, in descending order, so the biggest contours are listed first.
+ *
+ * To aid the user, a colored boxFit rectangle is drawn on the camera preview to show the location of each Blob
+ * The original Blob contour can also be added to the preview.  This is helpful when configuring the ColorBlobLocatorProcessor parameters.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@Autonomous(name = "colorLocator", group = "Concept")
+public class colorLocator extends LinearOpMode
+{
+    @Override
+    public void runOpMode()
+    {
+        autoPrototype prototype = new autoPrototype();
+        ColorBlobLocatorProcessor colorLocator = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(ColorRange.BLUE)         // use a predefined color match
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)    // exclude blobs inside blobs
+                .setRoi(ImageRegion.asUnityCenterCoordinates(-0.5, 0.5, 0.5, -0.5))  // search central 1/4 of camera view
+                .setDrawContours(true)                        // Show contours on the Stream Preview
+                .setBlurSize(5)                               // Smooth the transitions between different colors in image
+                .build();
+
+
+        VisionPortal portal = new VisionPortal.Builder()
+                .addProcessor(colorLocator)
+                .setCameraResolution(new Size(640, 360))
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .build();
+
+        telemetry.setMsTransmissionInterval(50);   // Speed up telemetry updates, Just use for debugging.
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
+
+        // WARNING:  To be able to view the stream preview on the Driver Station, this code runs in INIT mode.
+        while (opModeIsActive() || opModeInInit())
+        {
+            telemetry.addData("preview on/off", "... Camera Stream\n");
+
+            // Read the current list
+            List<ColorBlobLocatorProcessor.Blob> blobs = colorLocator.getBlobs();
+
+
+            ColorBlobLocatorProcessor.Util.filterByArea(50, 20000, blobs);  // filter out very small blobs.
+
+
+
+            telemetry.addLine(" Area Density Aspect  Center");
+
+            // Display the size (area) and center location for each Blob.
+            for(ColorBlobLocatorProcessor.Blob b : blobs)
+            {
+                RotatedRect boxFit = b.getBoxFit();
+                telemetry.addLine(String.format("%5d  %4.2f   %5.2f  (%3d,%3d)",
+                        b.getContourArea(), b.getDensity(), b.getAspectRatio(), (int) boxFit.center.x, (int) boxFit.center.y));
+                //left and right check
+                if(boxFit.center.x < 295) {
+                    telemetry.addLine("Move from left to center");
+                    //prototype.horizMove(-1,200);
+                } else if(boxFit.center.x > 355) {
+                    telemetry.addLine("Move from right to center");
+                    //prototype.horizMove(1,200);
+                }
+
+                if(b.getContourArea() > 13900) {
+                    telemetry.addLine("Too close to robot");
+                    //prototype.vertMove(-1,100);
+                } else if(b.getContourArea() < 3900) {
+                    telemetry.addLine("Too far from the robot");
+                    //prototype.vertMove(1,300);
+
+                }
+
+            }
+
+            telemetry.update();
+            sleep(50);
+        }
+    }
+}
