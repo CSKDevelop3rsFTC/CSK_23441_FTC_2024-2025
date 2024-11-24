2f5651d0a6b61125596e8f14e7f54debbf2c449b
p-anumarlu <68264610+p-anumarlu@users.noreply.github.com>
2024-11-23 20:42:34 -0800

    fully fixed git from messup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/rrAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/rrAuto.java
@@ -29,66 +29,102 @@ import org.firstinspires.ftc.teamcode.PinpointDrive;
 @Autonomous(name = "rrAuto", group = "Autonomous")
 public class rrAuto extends LinearOpMode {
 
+    public class claw {
+        private Servo clawServo;
+
+        public claw(HardwareMap hardwareMap) {
+            clawServo = hardwareMap.get(Servo.class, "clawServo");
+            clawServo.setDirection(Servo.Direction.FORWARD);
+        }
+
+        public class CloseClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                clawServo.setPosition(0.8);
+                return false;
+            }
+        }
+        public Action closeClaw() {
+            return new CloseClaw();
+        }
+
+        public class OpenClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                clawServo.setPosition(0.3);
+                return false;
+            }
+        }
+        public Action openClaw() {
+            return new OpenClaw();
+        }
+
+    }
+
     @Override
     public void runOpMode() {
         // instantiate your MecanumDrive at a particular pose.
         Pose2d beginPose = new Pose2d(-63, -11, 0);
         PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
+        claw clawServo = new claw(hardwareMap);
 
         waitForStart();
 
         Actions.runBlocking(
-                drive.actionBuilder(beginPose)
+                new SequentialAction(
 
-                        .setTangent(0)
-                        .strafeToConstantHeading(new Vector2d(-60,-57))
+                        //.setTangent(0)
+                        //.strafeToConstantHeading(new Vector2d(-60,-57))
+                        clawServo.openClaw(),
+                        clawServo.closeClaw()
 
                         /**
-                        .setTangent(0)
-                        .splineToConstantHeading(new Vector2d(-41,0), Math.PI * 2) // move up to center rungs
+                         .setTangent(0)
+                         .splineToConstantHeading(new Vector2d(-41,0), Math.PI * 2) // move up to center rungs
 
-                        .strafeTo(new Vector2d(-33, 0)) // go forward to line up specimen
+                         .strafeTo(new Vector2d(-33, 0)) // go forward to line up specimen
 
-                        .waitSeconds(2) // replace later with action of placing specimen
+                         .waitSeconds(2) // replace later with action of placing specimen
 
-                        .strafeTo(new Vector2d(-45, 0)) // move backwards a bit
+                         .strafeTo(new Vector2d(-45, 0)) // move backwards a bit
 
-                        .strafeTo(new Vector2d(-47, 49)) // move to sample one
+                         .strafeTo(new Vector2d(-47, 49)) // move to sample one
 
-                        .turnTo(Math.toRadians(-180)) // do a 180 to have intake facing sample
+                         .turnTo(Math.toRadians(-180)) // do a 180 to have intake facing sample
 
-                        .waitSeconds(2) // intake sample
+                         .waitSeconds(2) // intake sample
 
-                        .strafeTo(new Vector2d(-56.9, 57)) // move to dropping position
+                         .strafeTo(new Vector2d(-56.9, 57)) // move to dropping position
 
-                        .turnTo(Math.toRadians(130)) // turn around to the basket
+                         .turnTo(Math.toRadians(130)) // turn around to the basket
 
-                        .waitSeconds(2) // drop the pixels
+                         .waitSeconds(2) // drop the pixels
 
-                        .strafeTo(new Vector2d(-44.8, 61)) // move to sample position 2
+                         .strafeTo(new Vector2d(-44.8, 61)) // move to sample position 2
 
-                        .turnTo(Math.toRadians(-180))
+                         .turnTo(Math.toRadians(-180))
 
-                        .waitSeconds(2) // get the sample
+                         .waitSeconds(2) // get the sample
 
-                        .strafeTo(new Vector2d(-56.9, 57)) // move to dropping position
+                         .strafeTo(new Vector2d(-56.9, 57)) // move to dropping position
 
-                        .turnTo(Math.toRadians(130)) // turn around to the basket
+                         .turnTo(Math.toRadians(130)) // turn around to the basket
 
-                        .waitSeconds(2)
+                         .waitSeconds(2)
 
-                        .turnTo(Math.toRadians(0))
+                         .turnTo(Math.toRadians(0))
 
-                        .setTangent(0)
-                        .splineToConstantHeading(new Vector2d(0,-24), Math.PI / 2) // move up to center rungs
+                         .setTangent(0)
+                         .splineToConstantHeading(new Vector2d(0,-24), Math.PI / 2) // move up to center rungs
 
-                        //.setTangent(Math.PI / 2)
-                        //.setReversed(true)
-                        //.splineToConstantHeading(new Vector2d(0, 0), Math.PI / 2)
+                         //.setTangent(Math.PI / 2)
+                         //.setReversed(true)
+                         //.splineToConstantHeading(new Vector2d(0, 0), Math.PI / 2)
 
-                        **/
+                         **/
 
-                        .build());
+                )
+        );
 
     }
 
